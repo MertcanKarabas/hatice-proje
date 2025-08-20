312b0562802d545062722908c3d03db799fe9d12
Mertcan Karabaş <karabasmertcan@gmail.com>
2025-08-20 18:05:17 +0300

    bugfix

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -10,6 +10,7 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
+        origin: 'https://hatice-proje.vercel.app',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
     });
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    origin: 'https://hatice-proje.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
