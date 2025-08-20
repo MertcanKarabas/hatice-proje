287e0ec0d7ce6b086e9067a63b49d6def445b555
Mertcan Karabaş <karabasmertcan@gmail.com>
2025-08-21 01:41:31 +0300

    bugfix

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,30 +1,21 @@
-import { PrismaClient } from '../generated/prisma'; // Adjusted import path
-import * as bcrypt from 'bcrypt';
+import { PrismaClient } from '../generated/prisma';
 
 const prisma = new PrismaClient();
-const roundsOfHashing = 10;
 
 async function main() {
-    // Hash a password for your admin user
-    const password = await bcrypt.hash(
-        'adminHatice.', // <-- ‼️ CHANGE THIS to a secure password
-        roundsOfHashing,
-    );
-
-    // Create the admin user if they don't already exist
     const adminUser = await prisma.user.upsert({
         where: { email: 'admin@hatice.com' },
         update: {},
         create: {
             email: 'admin@hatice.com',
             name: 'Admin User',
-            password: password,
+            password: 'Haticeadmin.',
         },
     });
+
     console.log('Admin user has been created or already existed:', adminUser);
 }
 
-// Run the seed script
 main()
     .catch(async (e) => {
         console.error(e);
